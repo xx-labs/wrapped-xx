305ee482e986c9c6beaa736041c8e724f1583e8f
Bernardo Cardoso <bernardo@xx.network>
2024-02-23 21:44:24 +0000

    Update contract

--- contracts/WrappedXX.sol
+++ contracts/WrappedXX.sol
@@ -1,14 +1,26 @@
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
+contract WrappedXX is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-contract WrappedXX is ERC20PresetMinterPauser {
+    constructor(address admin) ERC20("Wrapped XX", "WXX") {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(MINTER_ROLE, admin);
+    }
 
-    /**
-     * @dev Builds ERC20PresetMinterPauser with name WrappedXX, symbol WXX, and decimals 9
-     */
-    constructor() public ERC20PresetMinterPauser("WrappedXX", "WXX") {
-        _setupDecimals(9);
+    // Minting function
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    // Decimals override
+    // (9 decimals to match native xx)
+    function decimals() public view virtual override returns (uint8) {
+        return 9;
     }
 }
